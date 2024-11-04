2a04b1601ac21f1570084afb7f789329b09dddc9
Ryan Makoni <rmakoni@1-C-3.42heilbronn.de>
2024-11-04 14:39:34 +0100

    main

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rmakoni <rmakoni@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 12:04:12 by rmakoni           #+#    #+#             */
-/*   Updated: 2024/11/04 11:59:03 by rmakoni          ###   ########.fr       */
+/*   Updated: 2024/11/04 14:30:41 by rmakoni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,16 +55,14 @@ char	*update_buffer(char *line_buffer)
 		return (NULL);
 	while (line_buffer[i] != '\0' && line_buffer[i] != '\n')
 		i++;
-	if (line_buffer[i] == '\n')
-		i++;
+	if (!line_buffer[i])
+		return (free(line_buffer), NULL);
+	i++;
 	if (!line_buffer[i])
 		return (free(line_buffer), NULL);
 	new_buffer = malloc((ft_strlen(&line_buffer[i]) + 1) * sizeof(char));
 	if (!new_buffer)
-	{
-		free(line_buffer);
-		return (NULL);
-	}
+		return (free(line_buffer), NULL);
 	j = 0;
 	while (line_buffer[i])
 		new_buffer[j++] = line_buffer[i++];
@@ -81,9 +79,9 @@ char	*read_file(int fd, char *line_buffer)
 		return (NULL);
 	temp_buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!temp_buffer)
-		return (free(temp_buffer), NULL);
+		return (NULL);
 	bytes_read = 1;
-	while (bytes_read > 0 && !ft_strchr(line_buffer, '\n'))
+	while (bytes_read > 0)
 	{
 		bytes_read = read(fd, temp_buffer, BUFFER_SIZE);
 		if (bytes_read == -1)
@@ -93,10 +91,9 @@ char	*read_file(int fd, char *line_buffer)
 		temp_buffer[bytes_read] = '\0';
 		line_buffer = ft_join_free(line_buffer, temp_buffer);
 		if (!line_buffer)
-		{
-			free(temp_buffer);
-			return (NULL);
-		}
+			return (free(temp_buffer), (NULL));
+		if (ft_strchr(line_buffer, '\n'))
+			break ;
 	}
 	return (free(temp_buffer), line_buffer);
 }
@@ -113,11 +110,32 @@ char	*get_next_line(int fd)
 		line_buffer = malloc(1 * sizeof(char));
 		if (!line_buffer)
 			return (NULL);
+		line_buffer[0] = '\0';
 	}
 	line_buffer = read_file(fd, line_buffer);
 	if (!line_buffer)
 		return (NULL);
 	line = get_my_line(line_buffer);
+	if (!line)
+		return (free(line_buffer), NULL);
 	line_buffer = update_buffer(line_buffer);
 	return (line);
 }
+// #include <dlfcn.h>
+// #include <fcntl.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <unistd.h>
+
+// int	main(void)
+// {
+// 	char	*line;
+// 	int		fd;
+
+// 	fd = open("Test.txt", O_RDONLY);
+// 	line = malloc(500);
+// 	line = get_next_line(fd);
+// 	line = get_next_line(fd);
+// 	printf("%s", line);
+// 	return (0);
+// }
